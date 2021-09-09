559d91367646b696b7121c3d8a54d80df5545da1
unknown <99067718@mydavinci.nl>
2021-09-09 09:33:28 +0200

    feestlunch & speelhaldag, zijn geupdated

--- Feestlunch.py
+++ Feestlunch.py
@@ -1,2 +1,3 @@
 calculated = 17 * 0.39 + 2 * 2.78 - 3 * 0.50
-print(calculated)
+txt = "De feestlunch kost je bij de bakker {} euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!"
+print(txt.format(calculated))
--- SpeelHalDag.py
+++ SpeelHalDag.py
@@ -1,3 +1,5 @@
 decreasednumber = 45/5
 finalNumber = 7.45 * 3 + decreasednumber * 0.37 * 3
-print(finalNumber)
+txt = "Dit geweldige dagje uit met 4 mensen in de speelhal met 45 minuten kost je maar {} euro."
+
+print(txt.format(finalNumber))
